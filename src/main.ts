5986a470618b9f2612e327dd9676b27b0fcccb3a
Haoquan Liang <haoquan@seas.upenn.edu>
2022-11-28 00:04:40 -0500

    GUI update

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,15 @@ import GltfRenderer from './gltf_renderer';
 import OrbitCamera from './orbit_camera';
 import FlyingCamera  from './flying_camera';
 import { mat4 } from 'gl-matrix';
+import * as DAT from 'dat.gui';
+
+const controls = {
+    instance_num: 10,
+    frequency: 1,
+    amplitude: 1,
+    air_density: 1,
+    phase_angle: 0,
+};
 
 export default class Application 
 {
@@ -49,6 +58,19 @@ export default class Application
 
         if(this.canRun)
         {
+            // GUI
+            const gui = new DAT.GUI();
+            gui.width = 300;
+            gui.add(controls, 'instance_num', 1, 100).step(1).name('Number of Butterflies')
+            .onChange(function(){
+                // TODO: update buffer
+            });
+            var forceGUI = gui.addFolder('Force');
+            forceGUI.add(controls,'frequency', 0, 1).step(0.01);
+            forceGUI.add(controls,'amplitude', 0, 4).step(1);
+            forceGUI.add(controls,'air_density',0, 2 ).step(0.1);
+            forceGUI.add(controls,'phase_angle',0, 360).step(1);
+
             // HTML stuff
             this.canvas = document.getElementById('gfx') as HTMLCanvasElement;
             this.canvas.width = this.canvas.height = 800;
@@ -66,11 +88,32 @@ export default class Application
             // Rigged Buffterfly (first renderer)
             let s : number = 1.5;
             this.gltf_butterfly = new GLTFGroup();
+
+            // create instancing name list and transform list
+            let instance_name = [];
+            let instance_trans = [];
+            for (let i=1; i<=controls.instance_num;++i)
+            {
+                let newName = ("b"+(i+1).toString());
+                instance_name.push(newName);
+                let even = (i % 2 == 0);
+                let newTrans = [];
+                if (even)
+                {
+                    newTrans = [s,0,0,0,  0,s,0,0,  0,0,s,0,  0 - 4 * i,0,0,1];
+                }
+                else
+                {
+                    newTrans = [s,0,0,0,  0,s,0,0,  0,0,s,0,  4 * i,0,0,1];
+                }
+                instance_trans.push(newTrans);
+            }
+
             await this.gltf_butterfly.init(
                 'https://raw.githubusercontent.com/Li-Jia-Jun/WebGPU-Butterfly/main/models/butterfly/butterfly-done.gltf',
-                2,
-                ["b1", "b2"],
-                [[s,0,0,0,  0,s,0,0,  0,0,s,0,  4,0,0,1], [s,0,0,0,  0,s,0,0,  0,0,s,0,  -4,0,0,1]]);
+                controls.instance_num,
+                instance_name,
+                instance_trans);
             this.renderer_butterfly = new GltfRenderer();
             await this.renderer_butterfly.init(this.adapter, this.device, this.queue, this.canvas, this.context, this.gltf_butterfly, this.depthTexture, this.depthTextureView, true);
 
