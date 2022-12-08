8c67abe38f3237f16378633467a172c44a5a5770
Haoquan Liang <haoquan@seas.upenn.edu>
2022-12-07 19:35:46 -0500

    Scene test

--- src/main.ts
+++ src/main.ts
@@ -131,7 +131,8 @@ export default class Application
         this.gltf_scene = new GLTFGroup();
         await this.gltf_scene.init(
             //'https://raw.githubusercontent.com/Li-Jia-Jun/WebGPU-Butterfly/main/models/forest_diorama/scene3.gltf',
-            'https://raw.githubusercontent.com/Li-Jia-Jun/WebGPU-Butterfly/main/models/trees_and_foliage/scene.gltf',
+            'https://raw.githubusercontent.com/Li-Jia-Jun/WebGPU-Butterfly/gltf-material/models/Camera/scene2.gltf',
+            //'https://raw.githubusercontent.com/Li-Jia-Jun/WebGPU-Butterfly/main/models/trees_and_foliage/scene.gltf',
             // 'https://raw.githubusercontent.com/Li-Jia-Jun/WebGPU-Butterfly/main/models/BoxTextured/glTF/BoxTextured.gltf',
             1,
             ['Scene'],
